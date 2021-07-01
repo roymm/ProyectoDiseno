f943595c2aa27e753f69fb06419ae21979086f6f
roymm <royamunoz@gmail.com>
2021-07-01 03:17:13 -0600

    Second version of CardVIP

--- src/Security_System/AccessCardDecorator.java
+++ src/Security_System/AccessCardDecorator.java
@@ -1,13 +1,17 @@
 package Security_System;
 
+/*Base class for all the decorators.
+Wraps an IAccessCard so the concrete decorators add additional functionalities.
+ */
 public abstract class AccessCardDecorator implements IAccessCard{
-    protected IAccessCard baseCard;
+    protected IAccessCard wrappedAccessCard;
 
-    public AccessCardDecorator(IAccessCard baseCard) {
-        this.baseCard = baseCard;
+    public AccessCardDecorator(IAccessCard wrappedAccessCard) {
+        this.wrappedAccessCard = wrappedAccessCard;
     }
 
+    @Override
     public boolean canAccess(Place place) {
-        return baseCard.canAccess(place);
+        return wrappedAccessCard.canAccess(place);
     }
 }
--- src/Security_System/CardVIP.java
+++ src/Security_System/CardVIP.java
@@ -1,13 +1,17 @@
 package Security_System;
 
+/* Represents an access card that can enter the VIP areas and
+the areas that the wrapped card from the super class can enter
+*/
 public class CardVIP extends AccessCardDecorator{
 
     public CardVIP(IAccessCard baseCard) {
         super(baseCard);
     }
 
+    //Decorates the canAccess method of the wrapped AccessCard with access to VIP areas
     @Override
     public boolean canAccess(Place place){
-        return place.getAccessLevel() == AccessLevel.VIP;
+        return this.wrappedAccessCard.canAccess(place) && place.getAccessLevel() == AccessLevel.VIP;
     }
 }
