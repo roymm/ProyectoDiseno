6a893d2dc311e7219d37754ae041222f95de6935
mj-chuz <medranom778@gmail.com>
2021-07-04 18:36:21 -0600

    Person class

--- src/Security_System/Person.java
+++ src/Security_System/Person.java
@@ -1,4 +1,16 @@
 package Security_System;
 
 public class Person {
+    private int idNumber;
+    private IAccessCard card;
+
+    public int getIdNumber()
+    {
+        return idNumber;
+    }
+
+    public IAccessCard getAccesCard()
+    {
+        return this.card;
+    }
 }
